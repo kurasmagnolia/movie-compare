0ccf4809ba1df9ebe4b907bdaead068346643a46
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-11 20:14:43 -0500

    added setLocalStorageKey and getLocalStorageKey

--- app/src/local-storage.js
+++ app/src/local-storage.js
@@ -1,1 +1,14 @@
 import defaultMovies from "../default-movies-data.json";
+
+const setLocalStorageKey = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getLocalStorageKey = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
